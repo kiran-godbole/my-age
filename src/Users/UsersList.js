2afb011d6cde9f226d9979a573941b497618db4c
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-07-22 13:15:41 +0530

    Display college name in UserList component

--- src/Users/UsersList.js
+++ src/Users/UsersList.js
@@ -2,18 +2,18 @@ import React from "react";
 
 import Card from "../Components/UI/Card";
 import classes from './UsersList.module.css';
-const UserList = (props) =>{
-    return(
+const UserList = (props) => {
+    return (
         <Card className={classes.users}>
-        <ul>
-            {props.user.map((user) => (
-                <li key={user.id}>
-                    {user.name} ({user.age} year old)
-                </li>
-            ))}
-        </ul>
+            <ul>
+                {props.user.map((user) => (
+                    <li key={user.id}>
+                        {user.name} ({user.age} year old ) {user.collage}
+                    </li>
+                ))}
+            </ul>
         </Card>
     )
 }
-        
+
 export default UserList;
