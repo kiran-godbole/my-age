0130c4be305e756b6e259f037e415c114475e371
Kiran Godbole <kiran.pgodbole@gmail.com>
2023-07-22 13:16:09 +0530

    Update prop name in App component to  college name

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import UserList from "./Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge, uCollage) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }]
+      return [...prevUsersList, { name: uName, age: uAge, collage: uCollage, id: Math.random().toString() }]
     });
   }
   return (
